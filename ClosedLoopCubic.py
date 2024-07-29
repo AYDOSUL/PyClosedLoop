e531fdf2717e3b2eef991ac717fc1fd343daf8be
AYDOSUL <sural.adyoth@gmail.com>
2024-07-29 16:43:55 -0400

    Added some additional features(for starting setpoints), made prints more useful

--- ClosedLoopCubic.py
+++ ClosedLoopCubic.py
@@ -17,21 +17,21 @@ while setpoint == 1: #Avoids div by zero errors
     setpoint = int(input('Setpoint for closed loop: ')) #Asking user for setpoint again
 cycle = int(input('Number of cycles the closed loop will optimize: ')) #Asking user for number of cycles the algorithm will run
 cval = 0 #Declaring initial correction value
-inval = 0 #Declaring initial input value
+inval = int(input('Custom starting input value: ')) #Declaring initial input value
 tuneval = 0.0001 #Declaring the tuning value for the loop
 i = 1 #Setting i to 1, avoiding division by zero errors(on line 18)
-
+print("#    Input    Output")
 while i < cycle+1 : 
     x = setpoint/i #Creating x variable to make rate of change smoother
     cval = (func(setpoint-x) - func(1))/(setpoint-1) #Creating calculation of the correction value
     if func(inval) < setpoint :
         inval = inval + tuneval*cval #Updating the input value as per calculation
-        print(inval) #Printing the updated input value
+        print(i," ",inval," ",func(inval)) #Printing the updated input value
     elif func(inval) > setpoint:
         inval = inval - tuneval*cval #Updating the input value as per calculation
-        print(inval) #Printing the updated input value
-    elif func(inval) == setpoint :
-        print(inval) #Printing the input value 
+        print(i," ",inval," ",func(inval)) #Printing the updated input value
+    elif func(func(inval)) == setpoint :
+        print(i," ",inval," ",func(inval)) #Printing the input value 
     i = i + 1
 print(func(inval)) #Printing final output of function
 print(inval) #Printing final input value
