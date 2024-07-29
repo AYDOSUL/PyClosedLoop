2a84a58814b4dd2f28fc523fae1c86ed8cf76b6c
AYDOSUL <sural.adyoth@gmail.com>
2024-07-29 10:11:02 -0400

    Cleaning up code and making the fuction choosable by user

--- ClosedLoopCubic.py
+++ ClosedLoopCubic.py
@@ -1,22 +1,17 @@
-import random as r #Importing required library
-a = r.randint(0,9) #Creating cubic coefficients
-b = r.randint(0,9)
-c = r.randint(0,9)
-d = r.randint(0,9)
+a = int(input('Coefficient of cubic equation(a): ')) #Asking user for the coefficients of the cubic equation
+b = int(input('Coefficient of cubic equation(b): '))
+c = int(input('Coefficient of cubic equation(c): '))
+d = int(input('Coefficient of cubic equation(d): '))
 
 def func(input):
-    return a + b*(input) + c*(input)*(input) + d*(input)*(input)*(input) #Creating the function
-print('Coefficients of cubic equation') #Printing the function coefficients
-print(a)
-print(b)
-print(c)
-print(d)
+    return d + c*(input) + b*(input)*(input) + a*(input)*(input)*(input) #Creating the function
+
 setpoint = int(input('Setpoint for closed loop: ')) #Asking user for setpoint
 cycle = int(input('Number of cycles the closed loop will optimize: ')) #Asking user for number of cycles the algorithm will run
 cval = 0 #Declaring initial correction value
 inval = 0 #Declaring initial input value
 tuneval = 0.0001 #Declaring the tuning value for the loop
-i = 1 #Setting i to 1, avoiding division by zero errors(on line 23)
+i = 1 #Setting i to 1, avoiding division by zero errors(on line 18)
 
 while i < cycle+1 : 
     x = setpoint/i #Creating x variable to make rate of change smoother
