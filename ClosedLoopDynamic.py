bed4ecf742885cb90afb20555dafab576c7a6ad2
AYDOSUL <sural.adyoth@gmail.com>
2024-08-01 22:49:37 -0400

    Optimized code and created new algorithm

--- ClosedLoopDynamic.py
+++ ClosedLoopDynamic.py
@@ -1,3 +1,4 @@
+#Dynamic Slope Approach(DSA)
 import random as r
 a = r.randint(-9,9) #Randomly assigning the coefficients
 b = r.randint(-9,9)
@@ -11,6 +12,7 @@ print(a)
 print(b)
 print(c)
 print(d)
+kB = 0.0001 #Setting DSA constant
 setpoint = int(input('Setpoint for closed loop: ')) #Asking user for setpoint
 while setpoint == 1: #Avoids div by zero errors
     print('setpoint cannot be 1')
@@ -19,18 +21,17 @@ cycle = int(input('Number of cycles the closed loop will optimize: ')) #Asking u
 cval = 0 #Declaring initial correction value
 inval = int(input('Custom starting input value: ')) #Declaring initial input value
 tol = float(input('Tolarance value for closed loop: '))
-tuneval = 0.0001 #Declaring the tuning value for the loop
 i = 1 #Setting i to 1, avoiding division by zero errors(on line 18)
 print("#    Input    Output")
 while i < cycle+1 : 
     x = setpoint/i #Creating x variable to make rate of change smoother
     cval = (func(setpoint-x) - func(1))/(setpoint-1) #Creating calculation of the correction value
     if func(inval) < setpoint :
-        inval = inval + tuneval*cval #Updating the input value as per calculation
+        inval = inval + kB*cval #Updating the input value as per calculation
         print(i," ",inval," ",func(inval)) #Printing the updated input value
         i = i + 1
     elif func(inval) > setpoint:
-        inval = inval - tuneval*cval #Updating the input value as per calculation
+        inval = inval - kB*cval #Updating the input value as per calculation
         print(i," ",inval," ",func(inval)) #Printing the updated input value
         i = i + 1
     elif func(inval)-tol < setpoint and func(inval)+tol > setpoint:
--- ClosedLoopLinear.py
+++ ClosedLoopLinear.py
@@ -1,3 +1,4 @@
+#Linear increase/decrease closed loop
 import random as r
 a = r.randint(0,9) #Randomly assigning the coefficients
 b = r.randint(0,9)
@@ -11,23 +12,22 @@ print(a)
 print(b)
 print(c)
 print(d)
+kP = 0.01 #Constant of proportion
 setpoint = int(input('Setpoint for closed loop: ')) #Asking user for setpoint
 cycle = int(input('Number of cycles the closed loop will optimize: ')) #Asking user for number of cycles the algorithm will run
-cval = 0 #Declaring initial correction value
-inval = 0
-i = 0 #Setting i to 1, avoiding division by zero errors(on line 18)
+inval = 0 
+i = 0
 print("#    Input    Output")
 while i < cycle : 
-    cval = 0.01
     if func(inval) < setpoint :
-        inval = inval + cval #Updating the input value as per calculation
+        inval = inval + kP #Updating the input value as per calculation
         print(i," ",inval," ",func(inval)) #Printing the updated input value
         i = i + 1
     elif func(inval) > setpoint:
-        inval = inval - cval #Updating the input value as per calculation
+        inval = inval - kP #Updating the input value as per calculation
         print(i," ",inval," ",func(inval)) #Printing the updated input value
         i = i + 1
-    elif func(inval)-tol < setpoint and func(inval)+tol > setpoint:
+    elif func(inval) == setpoint:
         print(i," ",inval," ",func(inval)) #Printing the input value 
         i = cycle+1
         
