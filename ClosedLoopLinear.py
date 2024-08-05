3a11df4d69201051cbd54e8240b6f39a4a52835c
AYDOSUL <sural.adyoth@gmail.com>
2024-08-04 20:34:53 -0400

    added negative positive adaptability

--- ClosedLoopLinear.py
+++ ClosedLoopLinear.py
@@ -1,9 +1,9 @@
 #Linear increase/decrease closed loop
 import random as r
-a = r.randint(0,9) #Randomly assigning the coefficients
-b = r.randint(0,9)
-c = r.randint(0,9)
-d = r.randint(0,9)
+a = r.randint(-9,9) #Randomly assigning the coefficients
+b = r.randint(-9,9)
+c = r.randint(-9,9)
+d = r.randint(-9,9)
 
 def func(input):
     return d + c*(input) + b*(input)*(input) + a*(input)*(input)*(input) #Creating the function
@@ -19,6 +19,10 @@ inval = 0
 i = 0
 print("#    Input    Output")
 while i < cycle : 
+    if func(inval) < func(inval + 1):
+            cval = kP
+    else:
+        cval = -kP
     if func(inval) < setpoint :
         inval = inval + kP #Updating the input value as per calculation
         print(i," ",inval," ",func(inval)) #Printing the updated input value
